5bbd709a4fbff67efcb9ec30fc55924799e79891
Onur Tosun <onurtosunn0@gmail.com>
2024-01-19 00:36:17 +0300

    Update txt_to_csv.py

--- txt_to_csv.py
+++ txt_to_csv.py
@@ -1,65 +1,74 @@
-"""
-Usage : python txt_to_csv.py -input /input/txt/path/ -output output/csv/path
-"""
+#!/usr/bin/env python
+#-*-coding:utf-8 -*-"
+'''
+@ Author: Onur Tosun
+@ Create Time : 2023-12-13
+@ Description : This script converts a txt file
+with bounding box annotations into the CSV format.
+'''
 
 import os
-import os.path
 import argparse
 import pandas as pd
 from PIL import Image
-from xml.dom.minidom import Document
+
+def read_labels(file_path):
+    with open(file_path, "r", encoding="ISO-8859-1") as file:
+        lines = file.read().split('\n')
+        return lines[:len(lines)-1]
 
 
-def write_to_csv(ann_path ,img_path ,dict):
-    
+def convert_annotations(annotations, img_size, dict):
+    w, h = img_size
+    converted_annotations = []
+
+    for annotation in annotations:
+        obj_parts = annotation.split(' ')
+        try:
+            class_name = dict[obj_parts[0]]
+        except KeyError as e:
+            print(f"Error: {e}. obj_parts[0]: {obj_parts[0]}")
+            continue
+        x1, y1, w1, h1 = map(float, obj_parts[1:])
+        
+        xmin = int((x1 * w) - (w1 * w) / 2.0)
+        ymin = int((y1 * h) - (h1 * h) / 2.0)
+        xmax = int((x1 * w) + (w1 * w) / 2.0)
+        ymax = int((y1 * h) + (h1 * h) / 2.0)
+
+        converted_annotations.append([class_name, xmin, ymin, xmax, ymax])
+
+    return converted_annotations
+
+def write_to_csv(ann_path, img_path, output_path, dict):
     annotations = []
-    for files in os.walk(ann_path):
-        for file in files[2]:
 
-            # Read image and get its size attributes
+    for root, dirs, files in os.walk(ann_path):
+        for file in files:
             img_name = os.path.splitext(file)[0] + '.jpg'
-            fileimgpath = os.path.join(img_path ,img_name)
-            im = Image.open(fileimgpath)
-            w = int(im.size[0])
-            h = int(im.size[1])
-
-            filelabel = open(os.path.join(ann_path , file), "r")
-            lines = filelabel.read().split('\n')
-            obj = lines[:len(lines)-1]  
+            img_path = os.path.join(img_path, img_name)
             
-            for i in range(0, int(len(obj))):
-                objbud=obj[i].split(' ')                
-                name = dict[objbud[0]]
-                x1 = float(objbud[1])
-                y1 = float(objbud[2])
-                w1 = float(objbud[3])
-                h1 = float(objbud[4])
+            with Image.open(img_path) as img:
+                img_size = img.size
 
-                xmin = int((x1*w) - (w1*w)/2.0)
-                ymin = int((y1*h) - (h1*h)/2.0)
-                xmax = int((x1*w) + (w1*w)/2.0)
-                ymax = int((y1*h) + (h1*h)/2.0)
+            labels = read_labels(os.path.join(ann_path, file))
+            converted_annotations = convert_annotations(labels, img_size, dict)
+            
+            for annotation in converted_annotations:
+                annotations.append([img_name, *img_size, *annotation])
 
-                annotations.append([img_name ,w ,h ,name ,xmin ,ymin ,xmax ,ymax])
-    column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax' ]
-    df = pd.DataFrame(annotations, columns=column_name)        
+    column_names = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
+    df = pd.DataFrame(annotations, columns=column_names)
+    df.to_csv(output_path, index=None)
     print(annotations[:10])
-    return df
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", required=True, help="input txt path")
+    parser.add_argument("-img", "--image", required=True, help="input images path")
+    parser.add_argument("-o", "--output", required=True, help="output csv path")
+    args = parser.parse_args()
 
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-i", "--input", required=True, help="input txt path")
-    ap.add_argument("-img", "--image", required=True, help="input images path")
-    ap.add_argument("-o", "--output", required=True, help="output csv path ")
-    args = vars(ap.parse_args()) 
+    class_dict = {'0': 'class1', '1': 'class2', '2': 'class3', '3': 'class4'}
 
-    dict = {'0' : 'class1',
-            '1': "class2"}      
-    
-    ann_path = args["input"]
-    img_path = args["image"]
-    csv_path = args["output"]  
-
-    data=write_to_csv(ann_path ,img_path  ,dict)      
-    data.to_csv(csv_path, index=None)
+    write_to_csv(args.input, args.image, args.output, class_dict)
